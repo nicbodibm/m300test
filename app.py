75068eae36df46de384a07860f0c3d6d7e2c07db
nicbodibm <133669734+nicbodibm@users.noreply.github.com>
2024-06-17 16:03:43 +0200

    Update app.py

--- app.py
+++ app.py
@@ -69,4 +69,6 @@ def contact():
     return render_template('contact.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)rue)
